0b416a36c3a87a05a5f3a3dab5ede65518ca83a9
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-08-22 23:30:18 +0530

    project structure ready

---
+++ apps/api/server.ts
@@ -0,0 +1,10 @@
+import { createServer } from "http";
+import app from "./src/app";
+
+const PORT = process.env.PORT || 5000;
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
