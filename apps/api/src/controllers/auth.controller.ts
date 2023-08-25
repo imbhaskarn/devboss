2d127c91aba3939b01d5f40a69fc7c4f8ebab4a0
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-08-25 22:27:20 +0530

    signup

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,8 +1,23 @@
 import { Request, Response } from 'express';
 import prisma from '../prisma';
 
-export const userSignUpController = (req: Request, res: Response) => {
+export const userSignUpController = async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
+    const user = await prisma.user.findFirst({
+        where: {
+          OR: [
+            { email: email },
+            { username: password },
+          ],
+        },
+      });
+
+    if (user)
+        return res.status(409).json({
+            status: 'error',
+            message: 'Email already registered.',
+        });
+        
 };
 
 export const userSignInController = (req: Request, res: Response) => {};
