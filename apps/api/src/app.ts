639645b3474c06a6690fcb1be04094a2d50b6dda
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-08-29 09:33:40 +0530

    format

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/user', userRouter)
+app.use('/api/v1/user', userRouter);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err.stack);
@@ -27,6 +27,5 @@ app.use('*', (req, res) => {
 });
 process.on('unhandledRejection', (reason: Error) => {
     console.log('Unhandled Promise Rejection: reason:');
-
 });
 export default app;
