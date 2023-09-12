b6035af973c164e83ab5db19ceab964ed16eedef
Bhaskar Sharma <imbhaskaran@gmail.com>
2023-09-12 22:15:43 +0530

    minor changes

--- apps/api/__tests__/app.test.ts
+++ apps/api/__tests__/app.test.ts
@@ -1,13 +1,13 @@
-import app from "../src/app";
-import request from "supertest";
+import app from '../src/app';
+import request from 'supertest';
 
-describe("User API", () => {
-  it("should return user data", async () => {
-    const response = await request(app).get("/status");
+describe('User API', () => {
+    it('should return user data', async () => {
+        const response = await request(app).get('/status');
 
-    // Assertions
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("status");
-    expect(response.body.status).toBe("OK");
-  });
+        // Assertions
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('status');
+        expect(response.body.status).toBe('OK');
+    });
 });
